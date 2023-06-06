49f25ebba90282030007069cc734659f30495652
gurneesh9 <c0859721@mylambton.ca>
2023-06-06 11:47:32 -0400

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const SERVER_HOST = "localhost"
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-
+//test
 //http://localhost:3000/
 app.get('/', function (req, res) {
     res.send("<h1>Home Page</h1>")
